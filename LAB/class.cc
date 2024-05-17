329b4f47e70784f3a86a59bb7f1a432095a663bc
gitsabdesktop <caprasab@gmail.com>
2024-05-18 02:06:09 +0900

    class

--- LAB/class.cc
+++ LAB/class.cc
@@ -3,20 +3,24 @@
 class TEST{
     public:
         int figure;
-
+        void calc(int temp);
+        void second();
 };
 
 void TEST::calc(int temp){
     figure = temp;
-    orubtf(%d\n,temp);
+    std::cout << figure <<std::endl;
+    second();
+}
+
+void TEST::second(){
+    std::cout << "second:" << figure <<std::endl;
 }
 
 
-
 int main(){
-    Test test;
+    TEST test;
     test.calc(10);
-    printf(%d\n, test.figure);
-
+    
     return 0;
 }
